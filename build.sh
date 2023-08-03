f15f04e2e9aed94d34787fad8b6a5ddfe427df23
Cyb3r-Jak3 <git@Cyberjake.xyz>
2023-08-03 17:48:16 -0400

    build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-pip install -r requirements.txt
+#!/bin/bash
+# pip install -r requirements.txt
 git submodule update --init
 wget -O stork https://files.stork-search.net/releases/v1.6.0/stork-ubuntu-20-04
 chmod +x stork
 export PATH=$PATH:$(pwd)
-openssl --version
+openssl version
 pelican content
