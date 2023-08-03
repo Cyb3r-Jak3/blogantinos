2ccf43780b61a1ab642cf71d132c1447abf7c404
Cyb3r-Jak3 <git@Cyberjake.xyz>
2023-08-03 17:43:51 -0400

    Add stork to path

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 pip install -r requirements.txt
 git submodule update --init
+wget -O stork https://files.stork-search.net/releases/v1.6.0/stork-ubuntu-20-04
+chmod +x stork
+export PATH=$PATH:$(pwd)
 pelican content
